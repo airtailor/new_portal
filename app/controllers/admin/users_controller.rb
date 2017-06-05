82bfdb6cc1d085e2e7da7d7612a9f5b11552b825
Jared Murphy <jaredcmurphy@gmail.com>
2017-06-05 11:48:26 -0400

    can create and update users from admin namespace

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,47 @@
+class Admin::UsersController < ApplicationController 
+  before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit, :update]
+  #before_action :is_admin
+
+  def index 
+    @users = User.all
+  end
+
+  def show 
+  end
+
+  def new 
+    @user = User.new 
+  end
+
+  def create 
+    @user = User.create(user_params)
+    if @user.save 
+      redirect_to [:admin, @user], notice: "User created successfully"
+    else 
+      render action: :new, alert: "Oops something went wrong"
+    end 
+  end
+
+  def edit
+  end
+
+  def update 
+    if @user.update_attributes(user_params)
+      redirect_to [:admin, @user], notice: "User successfully updated"
+    else 
+      render action: :edit, alert: "Oops something went wrong"
+    end
+  end
+
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
+  
+  namespace :admin  do 
+    get "/users", to: "users#index", as: :users
+    get "/users/new", to: "users#new", as: :new_user
+    get "/users/:id", to: "users#show", as: :user
+    get "/users/:id/edit", to: "users#edit", as: :edit_user
+    post "/users", to: "users#create" 
+    patch "/users/:id", to: "users#update"
+    delete "/users/:id", to: "users#destroy" 
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
