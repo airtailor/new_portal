df5a1bfbf9ea10e0e4aa331320987710d824ac6c
Jared Murphy <jaredcmurphy@gmail.com>
2017-06-05 19:00:11 -0400

    adds user tests for checking admin and deleting roles

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,4 +19,45 @@ RSpec.describe User, type: :model do
       expect(invalid_user).to_not be_valid
     end
   end
+
+  describe "admin?" do 
+    context "when user has the role 'admin'" do 
+      it "returns true" do 
+        admin_user = FactoryGirl.create(:user, email: Faker::Internet.email)
+        admin_user.add_role :admin
+
+        expect(admin_user.has_role? :admin).to eq(true)
+      end
+    end
+
+    context "when user does not have the role 'admin'" do 
+      it "returns false" do 
+        tailor_user = FactoryGirl.create(:user, email: Faker::Internet.email)
+        tailor_user.add_role :tailor
+
+        expect(tailor_user.has_role? :admin).to eq(false)
+      end
+    end
+  end
+
+  describe "delete_role" do 
+    it "deletes removes a specific role from the user" do 
+        admin_user = FactoryGirl.create(:user, email: Faker::Internet.email)
+        admin_user.add_role :admin
+        admin_user.add_role :tailor
+        admin_user.delete_role :tailor
+
+        expect(admin_user.has_role? :admin).to be(true)
+        expect(admin_user.has_role? :tailor).to be(false)
+    end
+
+    it "does not delete the role from the database" do 
+        admin_user = FactoryGirl.create(:user, email: Faker::Internet.email)
+        admin_user.add_role :admin
+        admin_user.add_role :tailor
+        admin_user.delete_role :tailor
+
+        expect(Role.count).to eq(2)
+    end
+  end
 end
