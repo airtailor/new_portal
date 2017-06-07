6e7777dfb02ba947ffd56631edf05910ecd449ef
Jared Murphy <jaredcmurphy@gmail.com>
2017-06-07 13:29:28 -0400

    adds passing integration tests for creating users

--- features/step_definitions/user_management_sets.rb
+++ features/step_definitions/user_management_sets.rb
@@ -1,22 +1,10 @@
 When(/^I create a new "([^"]*)" user$/) do |user_type|
-  if user_type == "admin"
-    email = "test@airtailor.com"
-    password = "airtailor"
-    non_role = :tailor
-  elsif user_type == "tailor"
-    email = "test@joestailor.com"
-    password = "joestailor"
-    non_role = :admin
-  else 
-    byebug
-  end
-
-  @current_user = {email: email, role: user_type}
+  user = FactoryGirl.attributes_for(:user)
 
   page.check user_type
-  fill_in "user_email", :with => email 
-  fill_in "user_password", :with => password
-  fill_in "user_password_confirmation", :with => password
+  fill_in "user_email", :with => user[:email]
+  fill_in "user_password", :with => user[:password]
+  fill_in "user_password_confirmation", :with => user[:password]
 end
 
 Then(/^the new user should have the correct "([^"]*)"$/) do |attribute|
@@ -24,5 +12,11 @@ Then(/^the new user should have the correct "([^"]*)"$/) do |attribute|
     User.last.has_role @current_user[attribute].first.name
   else
     User.last[attribute] == @current_user[attribute]
+  end
+end
+
+Then(/^I should see the list of the user's roles$/) do 
+  User.last.roles.each do |role|
+    page.should have_content(role.name.capitalize)
   end
 end
