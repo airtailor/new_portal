5517e801ceca73665865e9253c7cc0b768976858
Jared Murphy <jaredcmurphy@gmail.com>
2017-09-21 12:54:01 -0400

    refactor tests

--- spec/requests/api/orders_controller_spec.rb
+++ spec/requests/api/orders_controller_spec.rb
@@ -1,42 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe Api::OrdersController, type: :controller do
-  describe "GET #search" do
-    before :each do
-      co = FactoryGirl.create(:company, name: "Air Tailor")
-      @store = FactoryGirl.create(:retailer, name: "Air Tailor", company: co)
+  before :each do 
+      @airtailor_co = FactoryGirl.create(:company, name: "Air Tailor")
+      @airtailor_store = FactoryGirl.create(:retailer, name: "Air Tailor", company: @airtailor_co)
 
-      @user = FactoryGirl.create(:user, store: @store)
-      @auth_headers = @user.create_new_auth_token
-    end
+      @retailer_co = FactoryGirl.create(:company, name: "J.Crew")
+      @retailer_store = FactoryGirl.create(:retailer, name: "J.Crew - 5th Ave", company: @retailer_co)
 
-    it "returns a 200 ok" do
-      order = FactoryGirl.create(
+      @admin_user = FactoryGirl.create(:user, store: @airtailor_store)
+      @admin_user.add_role "admin"
+
+      @retailer_user = FactoryGirl.create(:user, store: @airtailor_store)
+      @retailer_user.add_role "retailer"
+
+      @auth_headers = @admin_user.create_new_auth_token
+     
+
+      # need to specify customer in this order so that we 
+      # use a unique and valid phone number, and 
+      # use the same first name so they both come up in the search
+      @customer_one = FactoryGirl.create(:customer, phone: 9045668701, first_name: "Jones")
+      @customer_two = FactoryGirl.create(:customer, phone: 6167804457, first_name: "Jones")
+      @order_one = FactoryGirl.create(
         :retailer_tailor_order,
-        retailer: @store 
+        retailer: @retailer_store,
+        customer: @customer_one
       )
 
-      get :search, {query: "#{order.customer.first_name}"}.merge(@auth_headers)
+      @order_two = FactoryGirl.create(
+        :retailer_tailor_order,
+        retailer: @airtailor_store,
+        customer: @customer_two
+      )
+
+      @order_three = FactoryGirl.create(
+        :retailer_tailor_order,
+        retailer: @airtailor_store,
+        customer: @customer_two
+      )
+  end
+
+  describe "GET #search" do
+    it "returns a 200 ok" do
+      get :search, {query: "#{@order_one.customer.first_name}"}.merge(@auth_headers)
       expect(response).to be_success
+    end
+
+    context "when the user is an admin" do 
+      it "allows them to search for other store's orders" do 
+        get :search, {query: "#{@order_one.customer.first_name}"}.merge(@auth_headers)
+        data = JSON.parse(response.body)
+        expect(data.count).to eq(Order.count)
+      end
     end
   end
 
   describe "GET #archived" do
-    before :each do
-      airtailor_co = FactoryGirl.create(:company, name: "Air Tailor")
-      store = FactoryGirl.create(:retailer, name: "Air Tailor", company: airtailor_co)
-
-      co = FactoryGirl.create(:company, name: "J.Crew")
-      @store = FactoryGirl.create(:retailer, name: "J.Crew - 5th Ave", company: co)
-
-      @user = FactoryGirl.create(:user, store: @store)
-      @auth_headers = @user.create_new_auth_token
-
-      order = FactoryGirl.create(
-        :retailer_tailor_order,
-        retailer: @store 
-      )
-      order.set_fulfilled
+   before :each do 
+      @order_two.set_fulfilled
+      @order_three.set_fulfilled
     end
 
     it "returns a 200 ok" do
@@ -44,24 +67,18 @@ RSpec.describe Api::OrdersController, type: :controller do
       expect(response).to be_success
     end
 
-    it "returns archived orders" do
+    it "returns fulfilled orders" do
       get :archived, {}.merge(@auth_headers)
-      fulfilled = JSON.parse(response.body).first["fulfilled"]
+      data = JSON.parse(response.body)
+      fulfilled = data.first["fulfilled"]
       expect(fulfilled).to be(true)
     end
 
     it "is ordered by recent fulfilled date" do
-      customer = FactoryGirl.create(:customer, phone: 6167804457)
-      order_two = FactoryGirl.create(
-        :retailer_tailor_order,
-        retailer: @store,
-        customer: customer
-      )
-      order_two.set_fulfilled
-
       get :archived, {}.merge(@auth_headers)
-      first_date = JSON.parse(response.body).first["fulfilled_date"]
-      second_date = JSON.parse(response.body).second["fulfilled_date"]
+      data = JSON.parse(response.body)
+      first_date = data.first["fulfilled_date"]
+      second_date = data.second["fulfilled_date"]
       expect(first_date > second_date).to be(true)
     end
   end
