2b262d64dbcc2e5975c461fc222d2026eff168f7
nialbima@gmail.com <nialbima@gmail.com>
2017-10-24 17:10:46 -0400

    added more constants files, refactored shipment (moved some stuff over to order) and backfilled a bunch of stuff

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,4 +1,6 @@
 class Shipment < ApplicationRecord
+  include ShipmentConstants
+
   validates :type, :shipping_label, :tracking_number, :weight, presence: true
 
   belongs_to :source, polymorphic: true
@@ -13,7 +15,7 @@ class Shipment < ApplicationRecord
 
   after_initialize :add_order_weight
   after_initialize :configure_shippo
-  after_create :text_all_shipment_customers
+  # after_create :text_all_shipment_customers
 
   private
 
@@ -29,55 +31,47 @@ class Shipment < ApplicationRecord
 
   def configure_shippo
 
+    # These should be autoloaded!
     Shippo.api_token = ENV["SHIPPO_KEY"]
     # Shippo.api_version = ENV["SHIPPO_API_VERSION"]
     Shippo.api_version = '2017-03-29'
 
-    to_address = source_address
-    from_address = destination_address
-    parcel = get_parcel
+    to, from, parcel = source_address, destination_address, get_parcel
 
-    Rails.logger.info "to_address: #{to_address}"
-    Rails.logger.info "from_address: #{from_address}"
+    Rails.logger.info "to: #{to}"
+    Rails.logger.info "from: #{from}"
     Rails.logger.info "parcel: #{parcel}"
 
-    shippo_shipment = create_shippo_shipment(to_address, from_address, parcel)
-    shippo_transaction = create_shippo_transaction(shippo_shipment)
-    add_shipping_label(shippo_transaction)
-    add_tracking_number(shippo_transaction)
+    shippo_shipment = Shippo::Shipment.create(
+      object_purpose: "PURCHASE",
+      address_from: from,
+      address_to: to,
+      parcels: parcel,
+      async: false
+    )
+
+    rate = get_shipping_rate(shippo_shipment)
+
+    shippo_transaction = Shippo::Transaction.create(
+      rate: rate, label_file_type: "PNG", async: false
+    )
+
+    self.shipping_label  = shippo_transaction[:label_url]
+    self.tracking_number = shippo_transaction[:tracking_number]
   end
 
-  def source_address(inhabitant)
-    source.for_shippo(inhabitant.address)
-    #
-    # if self.type == "OutgoingShipment"
-    #   # outgoing shipments always go to customer! :* )
-    #   if self.order.ship_to_store
-    #     get_retailer_address
-    #   else
-    #     get_customer_address
-    #   end
-    # elsif self.type == "IncomingShipment"
-    #   get_tailor_address if self.order.type == "TailorOrder"
-    #   get_retailer_address if self.order.type == "WelcomeKit"
-    # end
+  def source_address
+    source.for_shippo
   end
 
-  def destination_address(inhabitant)
-    destination.for_shippo(inhabitant.address)
-
-    # if self.type == "OutgoingShipment"
-    #   Rails.logger.info "GET SHIP FROM ADDRESS - OUTGOING SHIPMENT order type - #{self.order.type}"
-    #   return get_tailor_address if self.order.type == "TailorOrder"
-    #   return get_retailer_address if self.order.type == "WelcomeKit"
-    # elsif self.type == "IncomingShipment"
-    #   return get_customer_address
-    # end
+  def destination_address
+    destination.for_shippo
   end
 
   def get_parcel
-    if self.order.type == "WelcomeKit"
-      {
+    case order.type
+    when "WelcomeKit"
+      return {
         length: 6,
         width: 4,
         height: 1,
@@ -85,8 +79,8 @@ class Shipment < ApplicationRecord
         weight: 28,
         mass_unit: :g
       }
-    elsif self.order.type == "TailorOrder"
-       {
+    when "TailorOrder"
+       return {
         length: 7,
         width: 5,
         height: 3,
@@ -97,35 +91,8 @@ class Shipment < ApplicationRecord
     end
   end
 
-  def create_shippo_shipment(to_address, from_address, parcel)
-    shipment = Shippo::Shipment.create(
-      object_purpose: "PURCHASE",
-      address_from: from_address,
-      address_to: to_address,
-      parcels: parcel,
-      async: false
-    )
-  end
-
   def get_shipping_rate(rates)
     rates.find {|r| r.attributes.include? "BESTVALUE"}
   end
 
-  def create_shippo_transaction(shippo_shipment)
-    transaction = Shippo::Transaction.create(
-      #rate: shippo_shipment[:rates].first[:object_id],
-      #rate: shippo_shipment[:rates].find {|r| r.attributes.include? "BESTVALUE"},
-      rate: get_shipping_rate(shippo_shipment[:rates]),
-      label_file_type: "PNG",
-      async: false
-    )
-  end
-
-  def add_shipping_label(shippo_transaction)
-    self.shipping_label = shippo_transaction[:label_url]
-  end
-
-  def add_tracking_number(shippo_transaction)
-    self.tracking_number = shippo_transaction[:tracking_number]
-  end
 end
