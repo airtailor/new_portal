229d9ca8f9e7270d3df69b921f9a40d179fbb98a
nialbima@gmail.com <nialbima@gmail.com>
2017-10-30 19:44:21 -0400

    model changes are polluting this branch

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,30 +1,27 @@
 class Shipment < ApplicationRecord
   include ShipmentConstants
 
-  validates :type, :shipping_label, :tracking_number, :weight, presence: true
-  belongs_to :order
-
   after_initialize :add_order_weight
   after_initialize :configure_shippo
   after_create :send_text_to_customer
 
   # NOTE: Commented fields are in-flight. Below is the complete model.
 
-  # validates :shipment_type, inclusion: [ MAIL, MESSENGER ], presence: true
-  # validates :shipping_label, :tracking_number, :weight, presence: true,
-  #   if: :mail_shipment
+  validates :shipment_type, inclusion: [ MAIL, MESSENGER ], presence: true
+  validates :shipping_label, :tracking_number, :weight, presence: true,
+    if: :mail_shipment
 
-  # belongs_to :source, polymorphic: true
-  # belongs_to :destination, polymorphic: true
+  belongs_to :source, polymorphic: true
+  belongs_to :destination, polymorphic: true
 
-  # has_many :shipment_orders
-  # has_many :orders, through: :shipment_orders
+  has_many :shipment_orders
+  has_many :orders, through: :shipment_orders
 
-  # has_many :addresses, as: :source
-  # has_many :addresses, as: :destination
+  has_many :addresses, as: :source
+  has_many :addresses, as: :destination
 
 
-  # after_create :text_all_shipment_customers
+  after_create :text_all_shipment_customers
 
   # def request_messenger
   #   if is_messenger_shipment?
@@ -34,60 +31,60 @@ class Shipment < ApplicationRecord
   #   end
   # end
   #
-  # def create_label
-  #   # NOTE: this should work. A single source and single destination.
-  #   # But! it might not later!
-  #   if is_mail_shipment?
-  #     ShippoWorker.perform_async(shipment)
-  #   else
-  #     raise StandardError
-  #   end
-  # end
+  def create_label
+    # NOTE: this should work. A single source and single destination.
+    # But! it might not later!
+    if is_mail_shipment?
+      ShippoWorker.perform_async(shipment)
+    else
+      raise StandardError
+    end
+  end
   #
-  # def is_messenger_shipment?
-  #   self.shipment_type === MESSENGER
-  # end
+  def is_messenger_shipment?
+    self.shipment_type === MESSENGER
+  end
+
+  def is_mail_shipment?
+    self.shipment_type === MAIL
+  end
   #
-  # def is_mail_shipment?
-  #   self.shipment_type === MAIL
-  # end
+  def add_order_weight
+    # NOTE: we need to add weight to items.
+    # Weight comes in from Shopify or React, Rails does nothing.
+    self.weight = self.orders.sum(:weight)
+  end
   #
-  # def add_order_weight
-  #   # NOTE: we need to add weight to items.
-  #   # Weight comes in from Shopify or React, Rails does nothing.
-  #   self.weight = self.orders.sum(:weight)
-  # end
+  def text_all_shipment_customers
+    # this shouldn't be generic
+    orders.map(&:text_order_customers)
+  end
   #
-  # def text_all_shipment_customers
-  #   # this shouldn't be generic
-  #   orders.map(&:text_order_customers)
-  # end
-  #
-  # def get_parcel
-  #   # NOTE: This will break rapidly! We're defaulting to the first one because we don't
-  #   # bundle them yet.
-  #
-  #   case orders.first.type
-  #   when "WelcomeKit"
-  #     return {
-  #       length: 6,
-  #       width: 4,
-  #       height: 1,
-  #       distance_unit: :in,
-  #       weight: 28,
-  #       mass_unit: :g
-  #     }
-  #   when "TailorOrder"
-  #      return {
-  #       length: 7,
-  #       width: 5,
-  #       height: 3,
-  #       distance_unit: :in,
-  #       weight: self.order.weight,
-  #       mass_unit: :g
-  #     }
-  #   end
-  # end
+  def get_parcel
+    # NOTE: This will break rapidly! We're defaulting to the first one because we don't
+    # bundle them yet.
+
+    case orders.first.type
+    when "WelcomeKit"
+      return {
+        length: 6,
+        width: 4,
+        height: 1,
+        distance_unit: :in,
+        weight: 28,
+        mass_unit: :g
+      }
+    when "TailorOrder"
+       return {
+        length: 7,
+        width: 5,
+        height: 3,
+        distance_unit: :in,
+        weight: self.order.weight,
+        mass_unit: :g
+      }
+    end
+  end
 
 private
   def get_ship_to_address
@@ -132,16 +129,6 @@ private
   end
 
 
-  def send_text_to_customer
-    if ((self.order.retailer.name != "Air Tailor") && (self.type == "OutgoingShipment"))
-      customer = self.order.customer
-      customer_message = "Good news, #{customer.first_name.capitalize} -- your " +
-        "Airtailor Order (id: #{self.order.id}) is finished and is on its way to you! " +
-        "Here's your USPS tracking number: #{self.tracking_number}"
-      SendSonar.message_customer(text: customer_message, to: customer.phone)
-    end
-  end
-
   def configure_shippo
     Shippo.api_token = ENV["SHIPPO_KEY"]
     Shippo.api_version = '2017-03-29'
@@ -153,15 +140,10 @@ private
     puts "\n\n\n parcel #{parcel}"
     shippo_shipment = create_shippo_shipment(to_address, from_address, parcel)
     shippo_transaction = create_shippo_transaction(shippo_shipment)
-    add_shipping_label(shippo_transaction)
-    add_tracking_number(shippo_transaction)
+
+    self.shipping_label = shippo_transaction[:label_url]
+    self.tracking_number = shippo_transaction[:tracking_number]
   end
-
-
-  def add_order_weight
-    self.weight = self.order.weight
-  end
-
 
   def create_shippo_shipment(to_address, from_address, parcel)
     shipment = Shippo::Shipment.create(
@@ -185,40 +167,6 @@ private
       label_file_type: "PNG",
       async: false
     )
-  end
-
-  def add_shipping_label(shippo_transaction)
-    self.shipping_label = shippo_transaction[:label_url]
-  end
-
-  def add_tracking_number(shippo_transaction)
-    self.tracking_number = shippo_transaction[:tracking_number]
-  end
-
-  def get_parcel
-    # NOTE: This will break rapidly! We're defaulting to the first one because we don't
-    # bundle them yet.
-
-    case self.order.type
-    when "WelcomeKit"
-      return {
-        length: 6,
-        width: 4,
-        height: 1,
-        distance_unit: :in,
-        weight: 28,
-        mass_unit: :g
-      }
-    when "TailorOrder"
-       return {
-        length: 7,
-        width: 5,
-        height: 3,
-        distance_unit: :in,
-        weight: self.order.weight,
-        mass_unit: :g
-      }
-    end
   end
 
 end
