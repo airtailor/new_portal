bbab1ff7ed9e0dd0909f113308837dd14929968a
nialbima@gmail.com <nialbima@gmail.com>
2017-10-30 17:05:09 -0400

    removing from this branch

--- app/workers/shippo_worker.rb
+++ app/workers/shippo_worker.rb
@@ -1,38 +1,34 @@
 class ShippoWorker < ServiceWorker
-  include PostmatesHelper
-  # perform is defined on service_worker.
-  # if we don't want to make a generic object for external integrations,
-  # we can just move it all over here.
 
 
   def perform(shipment)
-
-    Shippo.api_token = ENV["SHIPPO_KEY"]
-    Shippo.api_version = ENV["SHIPPO_API_VERSION"]
-    # Shippo.api_version = '2017-03-29'
-
-    from = shipment.source.for_shippo
-    to   = shipment.destination.for_shippo
-
-    parcel = shipment.get_parcel
-
-    shippo = Shippo::Shipment.create(
-      object_purpose: "PURCHASE",
-      address_from: from,
-      address_to: to,
-      parcels: parcel,
-      async: false
-    )
-
-    rate = shippo.find {|r| r.attributes.include? "BESTVALUE"}
-
-    shippo_txn = Shippo::Transaction.create(
-      rate: rate, label_file_type: "PNG", async: false
-    )
-
-    shipment.shipping_label  = shippo_txn[:label_url]
-    shipment.tracking_number = shippo_txn[:tracking_number]
-
-    shipment.save
+    puts "SHIPPO WORKER"
+    # Shippo.api_token = ENV["SHIPPO_KEY"]
+    # Shippo.api_version = ENV["SHIPPO_API_VERSION"]
+    # # Shippo.api_version = '2017-03-29'
+    #
+    # from = shipment.source.for_shippo
+    # to   = shipment.destination.for_shippo
+    #
+    # parcel = shipment.get_parcel
+    #
+    # shippo = Shippo::Shipment.create(
+    #   object_purpose: "PURCHASE",
+    #   address_from: from,
+    #   address_to: to,
+    #   parcels: parcel,
+    #   async: false
+    # )
+    #
+    # rate = shippo.find {|r| r.attributes.include? "BESTVALUE"}
+    #
+    # shippo_txn = Shippo::Transaction.create(
+    #   rate: rate, label_file_type: "PNG", async: false
+    # )
+    #
+    # shipment.shipping_label  = shippo_txn[:label_url]
+    # shipment.tracking_number = shippo_txn[:tracking_number]
+    #
+    # shipment.save
   end
 end
