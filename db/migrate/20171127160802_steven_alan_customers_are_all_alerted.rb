4338679e5f2a1e36cb352f8f004d5949b03139d2
nialbima@gmail.com <nialbima@gmail.com>
2017-11-27 11:44:05 -0500

    tweak

--- db/migrate/20171127160802_steven_alan_customers_are_all_alerted.rb
+++ db/migrate/20171127160802_steven_alan_customers_are_all_alerted.rb
@@ -1,11 +1,7 @@
 class StevenAlanCustomersAreAllAlerted < ActiveRecord::Migration[5.1]
-  def up
+  # if we allow a down here, we'll be wiping data from after this merge
+  def change
     store = Retailer.where(id: 2).or(Retailer.where(name: "Steven Alan - Tribeca")).first
     store.orders.where(fulfilled: true).update_all(customer_alerted: true)
-  end
-
-  def down
-    # if we allow a down here, we'll wipe data from after this merge.
-    raise ActiveRecord::IrreversibleMigration
   end
 end
