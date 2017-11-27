e899cd61d7f733132479acd411fe6d1f3ab56f75
nialbima@gmail.com <nialbima@gmail.com>
2017-11-27 11:35:23 -0500

    migration for stevenalan

---
+++ db/migrate/20171127160802_steven_alan_customers_are_all_alerted.rb
@@ -0,0 +1,11 @@
+class StevenAlanCustomersAreAllAlerted < ActiveRecord::Migration[5.1]
+  def up
+    store = Retailer.where(id: 2).or(Retailer.where(name: "Steven Alan - Tribeca")).first
+    Customer.where(id: store.orders.select(:customer_id), fulfilled: true).update_all(customer_alerted: true)
+  end
+
+  def down
+    # if we allow a down here, we'll wipe data from after this merge.
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
