8bd4d4efa742976453e0353240bfaac31d58ae9a
nialbima@gmail.com <nialbima@gmail.com>
2017-12-01 16:32:39 -0500

    added authenticate_user back in

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::UsersController < ApplicationController
-  #before_action :authenticate_user!#, except: [:new, :create, :edit, :update]
+  before_action :authenticate_user!
   before_action :set_user, only: [:update_password]
 
   def update_password
