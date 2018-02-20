8c81eb31694dfe890e220e33e5d6918705aa1c79
Jared Murphy <jaredcmurphy@gmail.com>
2018-02-20 16:18:37 -0500

    adds activity constants file

---
+++ app/models/concerns/activity_constants.rb
@@ -0,0 +1,12 @@
+module ActivityConstants
+  SHIPMENT_CREATED    = 'shipment_created'
+  SHIPMENT_UPDATED    = 'shipment_updated'
+  SHIPMENT_DELIVERED  = 'shipment_delivered'
+
+  ORDER_CREATED       = 'order_created'
+  ORDER_CHECKED_IN    = 'order_checked_in'
+  ORDER_COMPLETED     = 'order_completed'
+  CUSTOMER_NOTIFIED   = 'customer_notified'
+  CUSTOMER_PICKED_UP  = 'customer_picked_up'
+  ORDER_NOTES_UPDATED = 'order_notes_updated'
+end
