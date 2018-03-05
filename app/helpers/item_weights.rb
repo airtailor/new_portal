98ec12b34d05582a75788c500576c85acbef9b6d
Jared Murphy <jaredcmurphy@gmail.com>
2018-03-05 18:27:42 -0500

    changes weight for pants

--- app/helpers/item_weights.rb
+++ app/helpers/item_weights.rb
@@ -2,7 +2,7 @@ module ItemWeights
   def self.get_weight_from_item_type_id(item_type_id)
     # id => weight
     weights = {
-      6 => 450,
+      6 => 680,
       7 => 225,
       10 => 340,
       13 => 340
