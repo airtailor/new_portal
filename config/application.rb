585e1b4377c67b15840ddcd7d2d6e874bbae4575
Brian Flynn <bdflynny@gmail.com>
2018-04-05 12:40:03 -0400

    adjust rack-cors gem

--- config/application.rb
+++ config/application.rb
@@ -11,23 +11,23 @@ module NewAirtailorPortal
     config.autoload_paths << File.join(Rails.root, 'app', 'classes')
     config.autoload_paths << File.join(Rails.root, 'app', 'classes', 'api')
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*',
-        :headers => :any,
-        :methods => [:get, :post, :options]
-      end
-    end
-
-    # config.middleware.use Rack::Cors do
+    # config.middleware.insert_before 0, Rack::Cors do
     #   allow do
     #     origins '*'
     #     resource '*',
-    #       :headers => :any,
-    #       :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-    #       :methods => [:get, :post, :options, :delete, :put]
+    #     :headers => :any,
+    #     :methods => [:get, :post, :options]
     #   end
     # end
+
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          :headers => :any,
+          :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+          :methods => [:get, :post, :options, :delete, :put]
+      end
+    end
   end
 end
