8ecd02aa794c9a9e43bba911eb295c66230c31a4
briandangerflynn <bdflynny@gmail.com>
2018-04-09 15:51:16 -0400

    remove uniquess of phone for customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-  validates :email, :phone, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
   validates :phone, presence: true
   validates :shopify_id, uniqueness: true, allow_blank: true
   validates :first_name, :last_name, presence: true
