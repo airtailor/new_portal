e9f4056918bb571cd9c40a19b4f676e625bcc10e
briandangerflynn <bdflynny@gmail.com>
2018-07-12 13:13:39 -0400

    remove Morgan's cell from new order texts

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
     airtailor_message = "Congrats :) An order with #{items.count} items" +
       " was just placed at #{order.retailer.name} for $#{order.total}!"
 
-    phone_list = ["6167804457", "9172321594", "9283008567"]
+    phone_list = ["6167804457", "9172321594"]
     phone_list.each do |phone|
       SendSonar.message_customer(text: airtailor_message, to: phone)
     end
